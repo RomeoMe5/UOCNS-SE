3c07e3da7c60eea658a518c3653dea2023f4baed
Mikhail Stepanov <mikhail.stepanovv@yandex.ru>
2020-04-12 15:05:04 +0300

    Initial commit

---
+++ src/main/java/ru/stepanov/uocns/UocnsApplication.java
@@ -0,0 +1,13 @@
+package ru.stepanov.uocns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UocnsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UocnsApplication.class, args);
+    }
+
+}
